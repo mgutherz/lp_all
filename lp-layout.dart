12dbc5bbec590cbb29c04bbb063077be78edaf2d
mgutherz <37817511+mgutherz@users.noreply.github.com>
2018-05-14 05:40:50 +0000

    Update lp-layout.dart

--- lp-layout.dart
+++ lp-layout.dart
@@ -12,3 +12,72 @@ class lpApp extends StatelessWidget {
     ); // MaterialApp
   } // build
 } // lpApp
+
+class GameScreen extends StatefulWidget {
+  @override
+  State createState() +> new GameScreenState();
+}
+
+class GameScreenState extends State<GameScreen> {
+  @override
+  Widget build(BuildContext context){
+    return new Scaffold( 
+      appBar: new AppBar(title: new Text("Friendlychat")),
+      body: new Column( // home page
+        children: <Widget>[
+          new Column( // Play Area
+            children: <Widget>[
+              new Row ( // Player 2 list built from Hand
+                children: <Widget>[
+                  new text(
+                    'Player2 row',
+                  ),
+                ],
+              ),
+              new Row( // Player 1 list build from Hand
+                children: <Widget>[
+                  new text(
+                    'Player1 row',
+                  ),
+                ],
+              ),
+              new Row( // current state
+                children: <Widget>[
+                  new text(
+                    'current state row',
+                  ),
+                ],
+              ),
+              new column( // game log list build from messages
+                children: <Widget>[
+                  new text(
+                    'log row',
+                  ),
+                ],
+              ),
+            ], // Play Area children
+          ),
+          new Column( // Control Area
+            children: <Widget>[ 
+              new Row( // inputs
+                children: <Widget>[
+                  new text(
+                    'inputs row',
+                  ),
+                ],
+              ),
+              new Row( // actions
+                children: <Widget>[
+                  new text(
+                    'actions row',
+                  ),
+                ],
+              ),
+            ], // control area children
+          ),
+        ]
+      ), // body
+    ); // Scaffold
+  } // build
+} // GameScreenState
+      
