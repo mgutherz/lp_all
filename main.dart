b09a02d6a6570b71a3664f0bd3f07af3395e7812
mgutherz <37817511+mgutherz@users.noreply.github.com>
2018-05-15 20:59:28 +0900

    Create main.dart

---
+++ main.dart
@@ -0,0 +1,129 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:layouttest/lp_player.dart';
+
+void main() {
+  runApp(new lpApp());
+}
+
+class lpApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: "lpApp",
+      theme: new ThemeData.dark(),
+      home: new GameScreen(),
+    ); // MaterialApp
+  } // build
+} // lpApp
+
+class GameScreen extends StatefulWidget {
+  @override
+  State createState() => new GameScreenState();
+}
+
+class GameScreenState extends State<GameScreen> {
+  int handSize = 8;
+  int maxCard = 9;
+
+  GameScreenState() {
+    handSize = 8;
+    maxCard = 9;
+  }
+
+  lpPlayer player = new lpPlayer(this.handSize, this.maxCard);
+  lpPlayer opponent = new lpPlayer(this.handSize, this.maxCard);
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Center(
+          child: new Text("Liars Poker"),
+        ),
+      ),
+      body: new Column(
+        // Home Page
+        children: <Widget>[
+          new Expanded(
+            child: new Column(
+              // play area
+              children: <Widget>[
+                opponent.build(context),
+                player.build(context),
+
+                new Row(
+                  // current state
+                  children: <Widget>[
+                    new Text(
+                      'current state row',
+                    ),
+                  ],
+                ),
+                new Column(
+                  // game log list build from messages
+                  children: <Widget>[
+                    new Text(
+                      'log row',
+                    ),
+                  ],
+                ),
+              ],
+            ), //Play Area
+          ),
+          new Column(
+            // Control Area
+            children: <Widget>[
+              new Row(
+                // inputs
+                children: <Widget>[
+                  new Text(
+                    'inputs row',
+                  ),
+                ],
+              ),
+              new Row(
+                // actions
+                children: <Widget>[
+                  new Text(
+                    'actions row',
+                  ),
+                ],
+              ),
+              new RaisedButton(
+                child: new Text(
+                  "Player",
+                  style: new TextStyle(color: Colors.white, fontSize: 20.0),
+                ),
+                color: Colors.red,
+                padding: const EdgeInsets.all(20.0),
+                onPressed: _doDealPlayer,
+              ),
+              new RaisedButton(
+                child: new Text(
+                  "Opponent",
+                  style: new TextStyle(color: Colors.white, fontSize: 20.0),
+                ),
+                color: Colors.blue,
+                padding: const EdgeInsets.all(20.0),
+                onPressed: _doDealOpponent,
+              ),
+            ], // control area children
+          ),
+        ],
+      ),
+    ); // Scaffold
+  } // build
+
+  void _doDealPlayer() {
+    setState(() {
+      player.deal(handSize, maxCard);
+    });
+  }
+  void _doDealOpponent() {
+    setState(() {
+      opponent.deal(handSize, maxCard);
+    });
+  }
+} // GameScreenState
