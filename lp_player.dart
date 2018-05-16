d540a270a47f37ba3f760e350fe5c40088e0632b
mgutherz <37817511+mgutherz@users.noreply.github.com>
2018-05-16 00:50:03 +0000

    Update lp_player.dart

--- lp_player.dart
+++ lp_player.dart
@@ -5,10 +5,29 @@ import 'package:flutter/material.dart';
 class lpPlayer extends StatelessWidget {
 
   lpPlayer(count, maxcard){
+    counts = new int[count];
+    bcount=0;
+     bcard=0;
+     scard=0;
+     scount=0;
+     lcard=0;
     this.deal(count, maxcard);
   }
 
   var Hand = new List();
+  lpPlayer nextPlayer;
+  int aiIndex;
+  int totalWins;
+  String playerName;
+  
+  // AI items
+  
+          int[] counts ;
+         int bcount;
+         int bcard;
+         int scard;
+         int scount;
+         int lcard;
 
   void deal(int count, int maxcard) {
     Hand.clear();
@@ -18,7 +37,76 @@ class lpPlayer extends StatelessWidget {
       Hand.add(r.nextInt(maxcard + 1));
     }
     Hand.sort();
+    
+    // 
+    if(aiIndex > 0){
+                        // find best, second best and liar card
+                        //System.out.println("init AI");
+                        bcount=0;
+                        scount=0;
+                        for (int i = 0; i<=maxcard; i++){
+                                opinions[i] = 0; // init opinions
+                                counts[i] = howMany(i);
+                                if(counts[i] >= bcount){
+                                        scard = bcard;
+                                        scount = bcount;
+                                        scard = bcard;
+                                        scount = bcount;
+                                        bcount = counts[i];
+                                        bcard = i;
+                                }else if(counts[i] >= scount){
+                                        scount = counts[i];
+                                        scard = i;
+                                }
+                                if((counts[i] == 0) && (Math.random()*2 > 1)) { // smooth distribution of selected lie
+                                        //System.out.printf("L changed to %d\n", i);
+                                        lcard=i;
+                                }
+                        }
+                }
+    
   }
+  
+  void setAIIndex(int index){
+                aiIndex = index;
+        }
+
+  int getAIIndex(){
+                return(aiIndex);
+        }
+
+  void setName(String name){
+                playerName = name;
+        }
+
+  String getName(){
+                return(playerName);
+        }
+
+  void setNext(lpPlayer nextplayer){
+                nextPlayer = nextplayer;
+        }
+
+  lpPlayer getNext(){
+                return(nextPlayer);
+        }
+         void addWin(int win){
+                totalWins += win;
+        }
+
+         int getWins(){
+                return(totalWins);
+        }
+  
+  int howMany(int input){
+                int count =0;
+                for (int card : Hand){
+                        if(card == input){
+                                count++;
+                        }
+                }
+                return(count);
+        }
 
   @override
   Widget build(BuildContext context) {
